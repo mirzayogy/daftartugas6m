cca5401a75ecbed8748f1d438f070fb3dd12ee1f
mirzayogy <mirza.yogy@gmail.com>
2022-04-17 09:47:10 +0800

    Generate SQL Login

--- sign-in/index.php
+++ sign-in/index.php
@@ -40,16 +40,20 @@
 <body class="text-center">
 
   <main class="form-signin">
-    <form>
+    <form method="post">
       <img class="mb-4" src="../assets/brand/bootstrap-logo.svg" alt="" width="72" height="57">
       <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
-
+      <?php
+      $inputSQL = "SELECT * FROM pengguna WHERE email ='" . $_POST['inputan_email'] . 
+      "' AND password = MD5('". $_POST['inputan_password'] ."')";
+      print_r($inputSQL);
+      ?>
       <div class="form-floating">
-        <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+        <input type="email" class="form-control" name="inputan_email" id="inputan_email" placeholder="name@example.com">
         <label for="floatingInput">Email address</label>
       </div>
       <div class="form-floating">
-        <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+        <input type="password" class="form-control" name="inputan_password" id="inputan_password" placeholder="Password">
         <label for="floatingPassword">Password</label>
       </div>
 
@@ -58,8 +62,8 @@
           <input type="checkbox" value="remember-me"> Remember me
         </label>
       </div>
-      <button class="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
-      <a href="../dashboard/index.html" class="w-100 btn btn-lg btn-warning">Ke Dashboard</a>
+      <button class="w-100 btn btn-lg btn-primary" name="button_signin" type="submit">Sign in</button>
+      <!-- <a href="../dashboard/index.html" class="w-100 btn btn-lg btn-warning">Ke Dashboard</a> -->
       <p class="mt-5 mb-3 text-muted">&copy; 2017–2021</p>
     </form>
   </main>
