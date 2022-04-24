6fd50abd88f275b09dcfba551f01dc532e59b8c8
mirzayogy <mirza.yogy@gmail.com>
2022-04-24 10:19:30 +0800

    table dan tombol

--- pages/dosen.php
+++ pages/dosen.php
@@ -1,1 +1,31 @@
 <h2>Dosen</h2>
+<div class="table-responsive">
+    <a href="#" class="btn btn-primary mb-3"> <span data-feather="plus"></span> Data Baru</a>
+    <table class="table table-striped table-sm">
+        <thead>
+            <tr>
+                <th scope="col">No</th>
+                <th scope="col">Nama Dosen</th>
+                <th scope="col">Handphone</th>
+                <th scope="col">Email</th>
+                <th scope="col">Opsi</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>1,001</td>
+                <td>random</td>
+                <td>data</td>
+                <td>placeholder</td>
+                <td>
+                    <a href="#" class="badge bg-warning">
+                        <span data-feather="edit"></span>
+                    </a>
+                    <a href="#" class="badge bg-danger">
+                        <span data-feather="x-circle"></span>
+                    </a>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+</div>
