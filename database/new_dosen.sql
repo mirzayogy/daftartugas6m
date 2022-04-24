a286a3d22179518273a6aedb4a8f658e568742ed
mirzayogy <mirza.yogy@gmail.com>
2022-04-24 10:35:14 +0800

    menampilkan data dosen

---
+++ database/new_dosen.sql
@@ -0,0 +1,1 @@
+CREATE TABLE `praktikum_daftartugas6m`.`dosen` ( `id` INT NOT NULL AUTO_INCREMENT , `nama_dosen` VARCHAR(255) NOT NULL , `handphone` VARCHAR(255) NULL , `email` VARCHAR(255) NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;
--- pages/dosen.php
+++ pages/dosen.php
@@ -12,20 +12,35 @@
             </tr>
         </thead>
         <tbody>
-            <tr>
-                <td>1,001</td>
-                <td>random</td>
-                <td>data</td>
-                <td>placeholder</td>
-                <td>
-                    <a href="#" class="badge bg-warning">
-                        <span data-feather="edit"></span>
-                    </a>
-                    <a href="#" class="badge bg-danger">
-                        <span data-feather="x-circle"></span>
-                    </a>
-                </td>
-            </tr>
+            <?php
+            include_once("../database/database.php");
+            $database = new Database;
+            $connection = $database->getConnection();
+
+            $selectSQL = "SELECT * FROM dosen";
+            $statement = $connection->prepare($selectSQL);
+            $statement->execute();
+
+            $no = 1;
+            while ($data = $statement->fetch(PDO::FETCH_ASSOC)) {
+            ?>
+                <tr>
+                    <td><?php echo $no++ ?></td>
+                    <td><?php echo $data['nama_dosen'] ?></td>
+                    <td><?php echo $data['handphone'] ?></td>
+                    <td><?php echo $data['email'] ?></td>
+                    <td>
+                        <a href="#" class="badge bg-warning">
+                            <span data-feather="edit"></span>
+                        </a>
+                        <a href="#" class="badge bg-danger">
+                            <span data-feather="x-circle"></span>
+                        </a>
+                    </td>
+                </tr>
+            <?php
+            }
+            ?>
         </tbody>
     </table>
 </div>
