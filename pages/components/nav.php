255161ef88d2627eb04ab93587ff6ef62b9b87b0
mirzayogy <mirza.yogy@gmail.com>
2022-04-24 09:46:52 +0800

    memecah komponen

---
+++ pages/components/nav.php
@@ -0,0 +1,30 @@
+<nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
+    <div class="position-sticky pt-3">
+        <ul class="nav flex-column">
+            <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="main.php">
+                    <span data-feather="home"></span>
+                    Dashboard
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="dosen.php">
+                    <span data-feather="file"></span>
+                    Dosen
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="matakuliah.php">
+                    <span data-feather="shopping-cart"></span>
+                    Mata Kuliah
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">
+                    <span data-feather="users"></span>
+                    Tugas
+                </a>
+            </li>
+        </ul>
+    </div>
+</nav>
