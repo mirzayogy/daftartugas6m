e9cdc8153f7e7cc65464841d7e9d1f35ff5c53dd
mirzayogy <mirza.yogy@gmail.com>
2022-04-24 09:30:49 +0800

    buat halaman main

--- sign-in/index.php
+++ sign-in/index.php
@@ -47,7 +47,7 @@
       session_start();
 
       if(isset($_SESSION['email'])){
-          header('Location: ../dashboard/index.php');
+          header('Location: ../pages/main.php');
       }
       include_once("../database/database.php");
       $database = new Database;
@@ -64,7 +64,7 @@
 
         if ($row_count > 0) {
           $_SESSION['email'] = $_POST['inputan_email'];
-          header('Location: ../dashboard/index.php')
+          header('Location: ../pages/main.php')
       ?>
           <div class="alert alert-success" role="alert">
             Login Berhasil
